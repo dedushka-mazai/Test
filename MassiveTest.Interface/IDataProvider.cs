c4a5094304b16ee79850059c1e4770691c6a421e
admin <admin@localhost.com>
2016-01-30 02:09:24 +0100

    Interfaces: IDataProvider interface added, file "Interfaces.cs" renamed to "IGraph.cs"

---
+++ MassiveTest.Interface/IDataProvider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MassiveTest.Interface
+{
+    /// <summary>
+    /// Represents class, which provide data for the graph
+    /// </summary>
+    public interface IDataProvider
+    {
+        /// <summary>
+        /// Fills graph with set of nodes
+        /// </summary>
+        /// <param name="graph">Graph instance to create nodes in</param>
+        void BuildGraph(IGraph graph);
+    }
+}
